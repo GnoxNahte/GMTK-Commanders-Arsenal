34c9b6a009c1ea0a4bacf596fd5e48867df90439
GnoxNahte <gnoxnahte.dev@gmail.com>
2023-07-08 12:11:16 +0800

    Add Player AI controls and movement and basic avoidance system.

--- Assets/Scripts/PlayerAI.cs
+++ Assets/Scripts/PlayerAI.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerAI : MonoBehaviour
 {
+    [SerializeField] private int avoidanceRadius;
+    [SerializeField] private ContactFilter2D contactFilter;
+
+    private PlayerControls playerMovement;
+
+    [SerializeField] [ReadOnly]
+    private List<Collider2D> enemies;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemies = new List<Collider2D>(50);
+
+        // Caching
+        playerMovement = GetComponent<PlayerControls>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int count = Physics2D.OverlapCircle(transform.position, avoidanceRadius, contactFilter, enemies);
+
+        Vector2 moveDir = Vector2.zero;
+
+        foreach (Collider2D enemy in enemies)
+        {
+            Vector2 offset = transform.position - enemy.transform.position;
+            moveDir += offset / Mathf.Max(offset.sqrMagnitude, 1);
+            print("OFfset: " + offset);
+        }
+
+        print("Move Dir: " + moveDir);
+
+        playerMovement.SetMoveDir(moveDir);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, avoidanceRadius);
     }
 }
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public enum AttackType
+    {
+        NotAttacking,
+        Attack1,
+        Attack2, 
+        Attack3,
+        Spin,
+        JumpFwd,
+        Cast1,
+        Cast2,
+    }
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private bool overrideControls;
+
+    [SerializeField]
+    [ReadOnly]
+    private Vector2 moveDir;
+
+    [SerializeField]
+    [ReadOnly]
+    private AttackType currAttackType;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sr;
+
+    // Animation id / hash
+    int animId_MoveSpd;
+    int animId_AttackType;
+    int animId_IsDead;
+
+    #region Public Methods
+    public void SetMoveDir(Vector2 dir)
+    {
+        moveDir = dir.normalized;
+    }
+
+    public void SetAttack(AttackType type)
+    {
+        animator.SetInteger(animId_AttackType, (int)type);
+    }
+
+    public void OnPlayerDead()
+    {
+        animator.SetBool(animId_IsDead, true);
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currAttackType = AttackType.NotAttacking;
+
+        // Caching
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        animId_MoveSpd = Animator.StringToHash("MoveSpd");
+        animId_AttackType = Animator.StringToHash("AttackType");
+        animId_IsDead = Animator.StringToHash("IsDead");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (overrideControls && input.sqrMagnitude > 0)
+        {
+            moveDir = input;
+        }
+
+        sr.flipX = moveDir.x < 0;
+
+        // Update animation
+        animator.SetFloat(animId_MoveSpd, moveDir.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * moveSpeed * Time.fixedDeltaTime);
+        //transform.position = rb.position + moveDir * moveSpeed * Time.fixedDeltaTime;
+    }
+
+
+}
